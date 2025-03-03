fa67078902fab499ac6d7791937566c3285aaac6
Jeremiah ANG (SP) <Jeremiah_ANG@SP.EDU.SG>
2025-03-03 11:53:14 +0800

    Remove helpers

--- Set A/helper.js
+++
@@ -1,7 +0,0 @@
-const inputs = [];
-const expected = [];
-for (let i = 1; i <= 50; i++) {
-    inputs.push([i]);
-    expected.push((i % 15 === 0 && 'foobar') || (i % 3 === 0 && 'foo') || (i % 5 === 0 && 'bar') || i);
-}
-console.log(inputs, expected);

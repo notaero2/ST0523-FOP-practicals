2ebc1a7eb0c334070591131aac2adb0033b82d02
Jeremiah Ang <jeremiah_ang@sp.edu.sg>
2025-03-11 10:11:41 +0800

    Update Q10

--- 2Operators/q10.js
+++ 2Operators/q10.js
@@ -1,11 +1,11 @@
 /**
  * 10 - Swap the values of num1 and num2
  */
-const num1 = 10;
-const num2 = 20;
+let a = 'a';
+let b = 'b';
 
 // You can use more variables if you need. (Hint: You need them)
 
-console.log('Q10 ' + num1 + ' ' + num2);
+console.log('Q10 ' + a + ' ' + b);
 
 // Run the program with `node ./q10.js`

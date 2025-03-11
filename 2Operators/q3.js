b832234e0f0b23f9dd11604ae28c0ca6d1f8df03
Jeremiah Ang <jeremiah_ang@sp.edu.sg>
2025-03-11 10:03:38 +0800

    Add equation2 to q3

--- 2Operators/q3.js
+++ 2Operators/q3.js
@@ -4,7 +4,8 @@
  *  Try: 8 / (2 * (2 + 2)), does it give you 16? or 1? Why?
  */
 
-const equation; // Calculate the equation here
-console.log("Q3: " + equation);
+const equation1; // Calculate the equation here
+const equation2; // Calculate the equation here
+console.log('Q3: ' + equation1 + ", " + equation2);
 
 // Run the program with `node ./q3.js`
